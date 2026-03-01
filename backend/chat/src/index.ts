2d142445967a2e8eeaf0f41c36f85b691ff51cac
sachin <sachin5565y@gmail.com>
2026-03-01 23:38:59 +0530

    created the config folder db connection

--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDb from './config/db.js';
 
 dotenv.config();
 const app = express();
 const port=process.env.PORT || 5002;
+
+connectDb();
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
