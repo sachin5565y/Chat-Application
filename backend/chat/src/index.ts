5afc256219c90dfa77c2fea8efa705544955fbaa
sachin <sachin5565y@gmail.com>
2026-03-15 21:21:21 +0530

    some minor changes

--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -1,23 +1,22 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDb from './config/db.js';
-import chatRoutes from './routes/chat.js';
-import isAuth from './middlewares/isAuth.js';
-import { createNewChat } from './controllers/chat.js';
-import router from './routes/chat.js';
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import chatRoutes from "./routes/chat.js";
+import cors from "cors";
+import { app, server } from "./config/socket.js";
 
 dotenv.config();
-const port=process.env.PORT || 5002;
+
 connectDb();
 
-
-const app = express();
-
 app.use(express.json());
+
+app.use(cors());
+
 app.use("/api/v1", chatRoutes);
 
+const port = process.env.PORT;
 
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
--- backend/mail/src/index.ts
+++ backend/mail/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import { startSendOtpConsumer } from "./consumer.js";
+import cors from "cors";
 dotenv.config();
 
 startSendOtpConsumer();
 const app=express();
+
+app.use(cors());
 
 app.listen(process.env.PORT || 5001,()=>{
     console.log("Server is running on port 5001");
